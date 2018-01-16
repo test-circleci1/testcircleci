f7976e144d25e6010796407aabadb90d73c6b4a7
kimura_yuuto <halu_k1126@yahoo.co.jp>
2018-01-16 10:08:28 +0900

    first

---
+++ CircleCI/SampleTest.php
@@ -0,0 +1,20 @@
+<?php
+require_once('./Sample.php');
+class SampleTest extends \PHPUnit\Framework\TestCase
+{
+    public function testHelloworld()
+    {
+        $Sample = new Sample();
+        $this->assertEquals($Sample->helloWorld(), 'hello world!');
+    }
+
+    /**
+     * @test
+     */
+    public function aaa()
+    {
+        $Sample = new Sample();
+        $this->assertEquals($Sample->helloWorld(), 'aaaaa');
+    }
+    
+}
